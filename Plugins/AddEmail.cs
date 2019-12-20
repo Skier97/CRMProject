ce579cd61a286dd9e2dd0a066abf6d6c2dfbc3b5
Skier97 <anton_ig@inbox.ru>
2019-12-20 12:34:04 +0300

    First version

---
+++ Plugins/AddEmail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+namespace Plugins
+{
+    public class AddEmail : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Получить контекст выполнения от поставщика услуг
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            // Коллекция InputParameters содержит все данные, переданные в запросе
+            Entity entity = (Entity)context.InputParameters["Target"];
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                
+                var email = entity.GetAttributeValue<string>("emailaddress1");
+                if (entity.LogicalName == "contact")
+                {
+                    //throw new InvalidPluginExecutionException((str==null).ToString());
+
+                    if ((email == null)||(email.Length == 0))
+                    {
+                        
+                        var contact = new Entity("contact");
+                        contact.Id = entity.Id;
+                        //throw new InvalidPluginExecutionException($"Почта контакта {contact.Id}не заполнена!");
+                        contact["emailaddress1"] = "jfjjdf@gnjfdk.tri";
+                        service.Update(contact);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Plugins/ExecutePosit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+namespace Plugins
+{
+    public class ExecutePosit : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Получить контекст выполнения от поставщика услуг
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            // Коллекция InputParameters содержит все данные, переданные в запросе
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                // Получить целевой объект из входных параметров
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                var position = entity.GetAttributeValue<string>("jobtitle");
+                if (entity.LogicalName == "contact")
+                {
+                    //throw new InvalidPluginExecutionException($"Должность {position.Length}контакта не заполнена!");
+                    //throw new InvalidPluginExecutionException((position=="").ToString()+position);
+                    if ((position == null)||(position.Length == 0))
+                    {
+                        throw new InvalidPluginExecutionException($"Должность контакта не заполнена!");
+                    } 
+                }
+            } 
+        }
+    }
+}
